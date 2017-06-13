9cbbb7f606d22161baef0551273d6aa42d934f20
AlexandreGuerra <agmguerra@gmail.com>
2017-06-12 21:51:12 -0300

    ajuste

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -7,6 +7,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     };
     console.log('Connected to MongoDB Server');
 
-    
+    db.collection('Todos').findOneAndUpdate({
+        _id: new ObjectID('593ee6c88eda7ce7e71b6ea2')
+    }, {
+        $set: {
+            completed: true
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    })
     //db.close();
 });
